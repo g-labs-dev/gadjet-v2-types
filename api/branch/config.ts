1bcc75ea8261161cd63e3692bb269f9fee3ce2a7
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-07-09 19:11:41 +0900

    feat: 설정 임시

---
+++ api/branch/config.ts
@@ -0,0 +1,8 @@
+import {} from "../../model";
+import {} from "../../type";
+
+type PARAMS = { hqId: number; branchId: number };
+
+// 설정
+// /hqs/0/branches/0/configs
+// ===========================

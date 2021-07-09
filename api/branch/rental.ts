d4be1665bf4383fea65dd5a134f791cdb9c7541f
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-07-09 15:53:49 +0900

    feat<api>: rental 추가

---
+++ api/branch/rental.ts
@@ -0,0 +1,73 @@
+import { RentalReservations, Rentals } from "../../model";
+import { Pagination } from "../../type";
+
+type PARAMS = { hqId: number; branchId: number };
+
+// 대관대여 조회
+// GET /hqs/0/branches/0/rentals
+export type GET_LIST_PARAMS = PARAMS;
+export type GET_LIST_QUERY = Pagination & { query: string };
+export type GET_LIST_RESPONSE = { total: number; rentals: Rentals[] };
+// ===========================
+
+// 대관대여 추가
+// POST /hqs/0/branches/0/rentals
+export type ADD_PARAMS = PARAMS;
+export type ADD_BODY = {
+  rental: Pick<Rentals, "price" | "name" | "description" | "startTime" | "endTime" | "availableCreditType" | "weekendFlag" | "shareFlag" | "imageId">;
+};
+export type ADD_RESPONSE = Rentals;
+// ===========================
+
+// 대관대여 수정
+// PUT /hqs/0/branches/0/rentals/0
+export type UPDATE_PARAMS = PARAMS & { rentalId: number };
+export type UPDATE_BODY = {
+  rental: Pick<Rentals, "price" | "name" | "description" | "startTime" | "endTime" | "availableCreditType" | "weekendFlag" | "shareFlag" | "imageId">;
+};
+export type UPDATE_RESPONSE = [number];
+// ===========================
+
+// 대관대여 제거
+// DEL /hqs/0/branches/0/rentals/0
+export type DELETE_PARAMS = PARAMS & { rentalId: number };
+export type DELETE_RESPONSE = [number];
+// ===========================
+
+// 대관대여 내역 조회
+// GET /hqs/0/branches/0/rentals/0/rental-resevations
+export type GET_RESERVATIONS_PARAMS = PARAMS & { rentalId: number };
+export type GET_RESERVATIONS_QUERY = Pagination & { date: [string, string] };
+export type GET_RESERVATIONS_RESPONSE = { total: number; rentalReservations: RentalReservations[] };
+// ===========================
+
+// 대관대여 내역 추가
+// POST /hqs/0/branches/0/rentals/0/rental-resevations
+export type ADD_RESERVATIONS_PARAMS = PARAMS & { rentalId: number };
+export type ADD_RESERVATIONS_BODY = {
+  rental: Pick<RentalReservations, "tenantId" | "userId" | "date" | "startTime" | "endTime" | "memo" | "usedCreditAmount" | "managerFlag">;
+};
+export type ADD_RESERVATIONS_RESPONSE = RentalReservations;
+// ===========================
+
+// 대관대여 내역 수정
+// PUT /hqs/0/branches/0/rentals/0/rental-resevations/0
+export type UPDATE_RESERVATIONS_PARAMS = PARAMS & { rentalId: number; rentalReservationId: number };
+export type UPDATE_RESERVATIONS_BODY = {
+  rental: Pick<RentalReservations, "tenantId" | "userId" | "date" | "startTime" | "endTime" | "memo" | "usedCreditAmount" | "managerFlag">;
+};
+export type UPDATE_RESERVATIONS_RESPONSE = [number];
+// ===========================
+
+// 대관대여 내역 제거
+// DEL /hqs/0/branches/0/rentals/0/rental-resevations/0
+export type DELETE_RESERVATIONS_PARAMS = PARAMS & { rentalId: number; rentalReservationId: number };
+export type DELETE_RESERVATIONS_RESPONSE = [number];
+// ===========================
+
+// 대관대여 내역 내보내기
+// GET /hqs/0/branches/0/rental-reservations
+export type EXPORT_RESERVATION_PARAMS = PARAMS;
+export type EXPORT_RESERVATION_QUERY = { date: [string, string] };
+export type EXPORT_RESERVATION_RESPONSE = any; // @@@@ xlsx
+// ===========================

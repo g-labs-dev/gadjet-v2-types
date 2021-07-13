1161fc11fccb1ef4f019b84ef564808d7e9fb133
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-07-13 12:42:43 +0900

    feat[api]: hq

---
+++ api/hq/index.ts
@@ -0,0 +1,19 @@
+import { Hqs } from "../../model";
+import {} from "../../type";
+
+type PARAMS = { hqId: number };
+
+// 운영사 조회
+// /hqs/0
+export type GET_DETAIL_PARAMS = PARAMS;
+export type GET_DETAIL_RESPONSE = Hqs;
+// ===========================
+
+// 운영사 수정
+// /hqs/0
+export type UPDATE_PARAMS = PARAMS;
+export type UPDATE_BODY = {
+  hq: Pick<Hqs, "email" | "name" | "logoImageId">;
+};
+export type UPDATE_REPONSE = [number];
+// ===========================

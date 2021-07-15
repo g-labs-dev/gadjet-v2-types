f2a7ab4843101ab3f96633c9965c7bafc15c92f4
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-07-15 17:40:40 +0900

    feat[api]: manager roles 수정

--- api/manager/role.ts
+++ api/manager/role.ts
@@ -1,4 +1,4 @@
-import { BranchRoles, HqRoles } from "../../model";
+import { Branches, Hqs } from "../../model";
 import {} from "../../type";
 
 type PARAMS = { managerId: number };
@@ -6,5 +6,5 @@ type PARAMS = { managerId: number };
 // 권한 조회
 // GET /managers/0/roles
 export type GET_PARAMS = PARAMS;
-export type GET_RESPONSE = { hqRoles: HqRoles[]; branchRoles: BranchRoles[] };
+export type GET_RESPONSE = { hqs: Hqs[]; branches: Branches[] };
 // ===========================

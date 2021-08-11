2e78c6168d2032081f2becbf63660c75ee56bd08
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-08-11 17:35:23 +0900

    feat[model]: BranchBusiness 컬럼 추가
    - type
    - class

--- model.ts
+++ model.ts
@@ -124,6 +124,8 @@ export type BranchBusiness = {
   identityNumber: string;
   businessNumber: string;
   corporationNumber: string;
+  type: string;
+  class: string;
   managers?: [{ name: string; department: string; tel: string; contact: string; email: string }, { name: string; department: string; tel: string; contact: string; email: string }];
 };
 

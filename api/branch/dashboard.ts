0e2362c9e70bc491c2535ddb6e8cfea399cb196f
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-08-20 18:27:10 +0900

    feat: dashboard event response 타입 수정

--- api/branch/dashboard.ts
+++ api/branch/dashboard.ts
@@ -6,7 +6,7 @@ type PARAMS = { hqId: number; branchId: number };
 // GET /hqs/0/branches/0/dashboard/events
 export type GET_EVENT_PARAMS = PARAMS;
 export type GET_EVENT_QUERY = { date: [string, string] }; // [YYYY-MM-DD, YYYY-MM-DD]
-export type GET_EVENT_RESPONSE = ({ date: string } & BranchDashboardDayEvent)[]; // YYYY-MM-DD
+export type GET_EVENT_RESPONSE = { date: string; event: BranchDashboardDayEvent }[]; // YYYY-MM-DD
 // ===========================
 
 // 이벤트 요약 조회

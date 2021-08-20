59947eee7a8e3fd315344f93fedcfa46df8ff0f9
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-08-20 14:59:57 +0900

    feat: addition 상세조회 api

--- api/branch/addition.ts
+++ api/branch/addition.ts
@@ -10,10 +10,10 @@ export type GET_LIST_QUERY = Pagination & { query: string };
 export type GET_LIST_RESPONSE = { total: number; additions: Additions[] };
 // ===========================
 
-// 부가서비스 조회
+// 부가서비스 상세 조회
 // GET /hqs/0/branches/0/additions/0
-export type GET_DETAIL_PARAMS = PARAMS & { additionId: number };
-export type GET_DETAIL_RESPONSE = { total: number; additions: Additions[] };
+export type GET_DETAIL_PARAMS = PARAMS;
+export type GET_DETAIL_RESPONSE = Additions;
 // ===========================
 
 // 부가서비스 추가

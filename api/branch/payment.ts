f9778c5560481c92a6d120628d95e6d509791090
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-08-23 14:35:26 +0900

    feat[api]: payment api 추가
    - 상세조회
    - 추가
    - 수정
    - 제거

--- api/branch/payment.ts
+++ api/branch/payment.ts
@@ -10,10 +10,40 @@ export type GET_LIST_QUERY = Pagination & { query: string; paymentType: PaymentT
 export type GET_LIST_RESPONSE = { total: number; payments: Payments[] };
 // ===========================
 
+// 결제내역 상세조회
+// GET /hqs/0/branches/0/payments/0
+export type GET_DETAIL_PARAMS = PARAMS & { paymentId: number };
+export type GET_DETAIL_RESPONSE = Payments;
+// ===========================
+
+// 결제내역 추가
+// POST /hqs/0/branches/0/payments
+export type ADD_PARAMS = PARAMS;
+export type ADD_BODY = {
+  payment: Pick<Payments, "item" | "price" | "approveDatetime" | "type" | "manualFlag" | "status">;
+};
+export type ADD_RESPONSE = Payments;
+// ===========================
+
+// 결제내역 수정
+// PUT /hqs/0/branches/0/payments/0
+export type UPDATE_PARAMS = PARAMS & { paymentId: number };
+export type UPDATE_BODY = {
+  payment: Pick<Payments, "item" | "price" | "approveDatetime" | "type" | "manualFlag" | "status">;
+};
+export type UPDATE_RESPONSE = [number];
+// ===========================
+
+// 결제내역 제거
+// DELETE /hqs/0/branches/0/payments/0
+export type DELETE_PARAMS = PARAMS & { paymentId: number };
+export type DELETE_RESPONSE = [number];
+// ===========================
+
 // 결제내역 요약
 // GET /hqs/0/branches/0/payments
 export type GET_SUMMARY_PARAMS = PARAMS;
-export type GET_SUMMARY_QUERY = { query: string; paymentType: PaymentType[]; };
+export type GET_SUMMARY_QUERY = { query: string; paymentType: PaymentType[] };
 export type GET_SUMMARY_RESPONSE = PaymentSummary;
 // ===========================
 

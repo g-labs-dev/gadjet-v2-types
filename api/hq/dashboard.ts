f56ce687d1ba63b0428779f1dae8ba28ac97e5dd
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-08-26 17:37:41 +0900

    feat[api]: hq dashboard 타임명 수정

--- api/hq/dashboard.ts
+++ api/hq/dashboard.ts
@@ -5,40 +5,40 @@ type PARAMS = { hqId: number };
 
 // 최근 추가된 계약 조회 (20개)
 // GET /hqs/0/dashboard/contracts/recent
-export type DASHBOARD_CONTRACT_RECENT_PARAMS = PARAMS;
-export type DASHBOARD_CONTRACT_RECENT_QUERY = { pageSize: number };
-export type DASHBOARD_CONTRACT_RECENT_RESPONSE = Contracts[];
+export type GET_RECENT_CONTRACT_PARAMS = PARAMS;
+export type GET_RECENT_CONTRACT_QUERY = { pageSize: number };
+export type GET_RECENT_CONTRACT_RESPONSE = Contracts[];
 /**
- * order by startDate desc
+ * order by createdAt desc
  */
 // ===========================
 
 // 전 지점 계약 검색
 // GET /hqs/0/dashboard/contracts/search
-export type DASHBOARD_CONTRACT_SEARCH_PARAMS = PARAMS;
-export type DASHBOARD_CONTRACT_SEARCH_QUERY = Pagination & { query: string };
-export type DASHBOARD_CONTRACT_SEARCH_RESPONSE = { total: number; contracts: Contracts[] };
+export type SEARCH_CONTRACT_PARAMS = PARAMS;
+export type SEARCH_CONTRACT_QUERY = Pagination & { query: string };
+export type SEARCH_CONTRACT_RESPONSE = { total: number; contracts: Contracts[] };
 // ===========================
 
 // 시작계약
 // GET /hqs/0/dashboard/contracts/start
-export type DASHBOARD_CONTRACT_START_PARAMS = PARAMS;
-export type DASHBOARD_CONTRACT_START_QUERY = { date: string };
-export type DASHBOARD_CONTRACT_START_RESPONSE = Contracts[];
+export type GET_START_CONTRACT_PARAMS = PARAMS;
+export type GET_START_CONTRACT_QUERY = { date: string };
+export type GET_START_CONTRACT_RESPONSE = Contracts[];
 // ===========================
 
 // 종료계약
 // GET /hqs/0/dashboard/contracts/end
-export type DASHBOARD_CONTRACT_END_PARAMS = PARAMS;
-export type DASHBOARD_CONTRACT_END_QUERY = { date: string };
-export type DASHBOARD_CONTRACT_END_RESPONSE = Contracts[];
+export type GET_END_CONTRACT_PARAMS = PARAMS;
+export type GET_END_CONTRACT_QUERY = { date: string };
+export type GET_END_CONTRACT_RESPONSE = Contracts[];
 // ===========================
 
 // 미납 청구서
 // GET /hqs/0/dashboard/bills/unpaid
-export type DASHBOARD_BILL_UNPAID_PARAMS = PARAMS;
-export type DASHBOARD_BILL_UNPAID_QUERY = { date: string };
-export type DASHBOARD_BILL_UNPAID_RESPONSE = Bills[];
+export type GET_UNPAID_BILL_PARAMS = PARAMS;
+export type GET_UNPAID_BILL_QUERY = { date: string };
+export type GET_UNPAID_BILL_RESPONSE = Bills[];
 /**
  * Bills.paymentEndDate < date && Bills.unpaid > 0
  */
@@ -46,9 +46,9 @@ export type DASHBOARD_BILL_UNPAID_RESPONSE = Bills[];
 
 // 전 지점 공간 조회
 // GET /hqs/0/dashboard/spaces
-export type DASHBOARD_SPACE_PARAMS = PARAMS;
-export type DASHBOARD_SPACE_QUERY = { date: string };
-export type DASHBOARD_SPACE_RESPONSE = Spaces[];
+export type GET_SPACE_PARAMS = PARAMS;
+export type GET_SPACE_QUERY = { date: string };
+export type GET_SPACE_RESPONSE = Spaces[];
 /**
  * Bills.contracts = Contracts.suspendDate > date
  */

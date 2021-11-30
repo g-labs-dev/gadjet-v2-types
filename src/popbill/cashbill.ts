edeca6c9686fe7c6ba0bd5ddf2ae92077f49a523
이태호 <rami9014@gmail.com>
2021-11-30 13:55:31 +0900

    feat[model] : 현금영수증 취소에 쓰일 컬럼 추가

--- src/popbill/cashbill.ts
+++ src/popbill/cashbill.ts
@@ -1,28 +1,31 @@
 // https://docs.popbill.com/taxinvoice/node/api
 
 export type Cashbill = {
-  mgtKey: string;
-  tradeType: "승인거래" | "취소거래";
-  tradeUsage: "소득공제용" | "지출증빙용";
-  identityNum: string;
-  taxationType: "과세" | "비과세";
+  mgtKey: string
+  tradeType: '승인거래' | '취소거래'
+  tradeUsage: '소득공제용' | '지출증빙용'
+  identityNum: string
+  taxationType: '과세' | '비과세'
 
-  totalAmount: string;
-  supplyCost: string;
-  tax: string;
-  serviceFee: string;
+  totalAmount: string
+  supplyCost: string
+  tax: string
+  serviceFee: string
 
-  franchiseCorpNum: string;
-  franchiseCorpName?: string;
-  franchiseCEOName?: string;
-  franchiseAddr?: string;
-  franchiseTEL?: string;
+  franchiseCorpNum: string
+  franchiseCorpName?: string
+  franchiseCEOName?: string
+  franchiseAddr?: string
+  franchiseTEL?: string
 
-  customerName?: string;
-  itemName?: string;
-  orderNumber?: string;
-  email?: string;
-  hp?: string;
-};
+  customerName?: string
+  itemName?: string
+  orderNumber?: string
+  email?: string
+  hp?: string
 
-export type CashbillCancel = Cashbill & { orgConfirmNum: string; orgTradeDate: string; cancelType: 1 | 2 | 3 };
+  isPartCancel?: number
+  cancelType?: number
+}
+
+export type CashbillCancel = Cashbill & { orgConfirmNum: string; orgTradeDate: string; cancelType: 1 | 2 | 3 }

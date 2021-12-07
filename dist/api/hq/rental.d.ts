65fa886fd6e5576dc8df9db52ac8d6e19001c2da
Kang Byung Seon <rkdqudtjs1@naver.com>
2021-12-07 14:03:55 +0900

    feat[api]: 운영사 대관/대여 조회 Pagination

--- dist/api/hq/rental.d.ts
+++ dist/api/hq/rental.d.ts
@@ -1,9 +1,10 @@
 import { Rentals } from '../../model';
+import { Pagination } from '../../type';
 declare type PARAMS = {
     hqId: number;
 };
 export declare type GET_RENTALS_PARAMS = PARAMS;
-export declare type GET_RENTALS_QUERY = {};
+export declare type GET_RENTALS_QUERY = Pagination & {};
 export declare type GET_RENTALS_RESPONSE = {
     total: number;
     rentals: Rentals[];
--- src/api/hq/rental.ts
+++ src/api/hq/rental.ts
@@ -1,11 +1,11 @@
 import { Rentals } from '../../model'
-import {} from '../../type'
+import { Pagination } from '../../type'
 
 type PARAMS = { hqId: number }
 
 // 운영사 대관/대여 목록 조회
 // GET /hqs/0/rentals
 export type GET_RENTALS_PARAMS = PARAMS
-export type GET_RENTALS_QUERY = {}
+export type GET_RENTALS_QUERY = Pagination & {}
 export type GET_RENTALS_RESPONSE = { total: number; rentals: Rentals[] }
 // ===========================

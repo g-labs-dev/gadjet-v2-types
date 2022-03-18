6ad74f1f75a29b1af7391442aaae5e3431e3b5d8
Kang Byung Seon <rkdqudtjs1@naver.com>
2022-03-18 11:06:30 +0900

    feat: search 추가

---
+++ dist/type/search.d.ts
@@ -0,0 +1,78 @@
+import { BillType, ContractStatus, ExpenditureType, PaymentStatus, PaymentType } from '.';
+export declare type ContractSearch = {
+    query: string;
+    spaceTypes: number[];
+    status: ContractStatus[];
+};
+export declare type BillSearch = {
+    query: string;
+    startDate: [string, string];
+    spaceTypes: number[];
+    type: BillType[];
+};
+export declare type ReceiptSearch = {
+    query: string;
+    requestDatetime: [string, string];
+};
+export declare type SpaceSearch = {
+    query: string;
+    emptyFlag: boolean;
+};
+export declare type RentalSearch = {
+    query: string;
+};
+export declare type RentalReservationSearch = {
+    date: [string, string];
+    rentalIds: number[];
+};
+export declare type TenantSearch = {
+    query: string;
+    contractStatus: ContractStatus[];
+};
+export declare type NoticeSearch = {
+    query: string;
+    writeDate: [string, string];
+};
+export declare type AccessLogSearch = {
+    query: string;
+    datetime: [string, string];
+};
+export declare type ProductSearch = {
+    query: string;
+};
+export declare type ProductSaleSearch = {
+    query: string;
+    datetime: [string, string];
+    productIds: number[];
+};
+export declare type ServiceSearch = {
+    query: string;
+};
+export declare type ServiceSaleSearch = {
+    query: string;
+    datetime: [string, string];
+    serviceIds: number[];
+};
+export declare type ExpenditureSearch = {
+    query: string;
+    expenditureTypes: ExpenditureType[];
+    date: [string, string];
+};
+export declare type PaymentSearch = {
+    query: string;
+    paymentType: PaymentType[];
+    paymentStatus: PaymentStatus[];
+    date: [string, string];
+};
+export declare type EventSearch = {
+    query: string;
+};
+export declare type AdditionSearch = {
+    query: string;
+};
+export declare type BranchRoleSearch = Record<string, unknown>;
+export declare type CashSearch = {
+    query: string;
+    date: [string, string];
+};
+//# sourceMappingURL=search.d.ts.map

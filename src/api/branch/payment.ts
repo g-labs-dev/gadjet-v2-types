9347b0592562c1337b24b72f2b2c00d208377d74
ramiTae <ramiTae.dev@gmail.com>
2022-07-19 11:16:01 +0900

    feat: 청구서 결제내역 내려받기 api 추가

--- src/api/branch/payment.ts
+++ src/api/branch/payment.ts
@@ -51,7 +51,7 @@ export type GET_SUMMARY_RESPONSE = PaymentSummary
 // 결제내역 내보내기
 // GET /hqs/0/branches/0/payments/export
 export type EXPORT_PARAMS = PARAMS
-export type EXPORT_QUERY = { query: string; paymentType: PaymentType[]; paymentStatus: PaymentStatus[] }
+export type EXPORT_QUERY = PaymentSearch
 export type EXPORT_RESPONSE = Buffer
 // ===========================
 
